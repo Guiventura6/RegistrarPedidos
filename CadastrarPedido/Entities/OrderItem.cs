c87ee48608869814a08b13ec12b2c2745ffa20da
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2021-12-27 20:20:48 -0300

    Criacao da classe OrderItem

---
+++ CadastrarPedido/Entities/OrderItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastrarPedido.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int quantity, double price)
+        {
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public double SubTotal()
+        {            
+            return Quantity * Price;
+        }
+    }
+}
