cc4eba509277a16c2bd44b134a6abed5f84aded5
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2021-12-27 20:29:29 -0300

    Criacao da classe Order

---
+++ CadastrarPedido/Entities/Order.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using CadastrarPedido.Entities.Enums;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastrarPedido.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, List<OrderItem> items)
+        {
+            Moment = moment;
+            Status = status;
+            Items = items;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+
+    }
+}
