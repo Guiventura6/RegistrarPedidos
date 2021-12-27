64cfc707ae360df61d240effd73604050d14d732
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2021-12-27 20:40:11 -0300

    Implementacao da etapada de instanciacao da classe Client no arquivo program.

--- CadastrarPedido/Program.cs
+++ CadastrarPedido/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CadastrarPedido.Entities;
+using System;
 
 namespace CadastrarPedido
 {
@@ -6,7 +7,19 @@ namespace CadastrarPedido
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+            Console.WriteLine($"{client.Name}, {client.Email}, {client.BirthDate}");
+
+            Console.WriteLine();
+            Console.Write("Enter order data:");
         }
     }
 }
